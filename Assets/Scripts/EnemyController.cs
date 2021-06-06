c4a63a2c4472dd339ac8f58f7cce9a170dbfd389
gustavolucero1989 <gustavolucero1@hotmail.com>
2021-06-06 20:25:37 -0300

    Beta v1.0

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public float velocity = 2f;
+	private Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		rigidbody2D.velocity = Vector2.left * velocity;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Destroyer") {
+			Destroy(gameObject);
+		}
+	}
+}
